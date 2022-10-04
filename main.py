71c9081ae29353b737210015df2249e3017becd9
tunatuna tunatuna <113930372+Maverickstomcats@users.noreply.github.com>
2022-10-04 17:25:49 +0000

    aaaaaaaaaaa

---
+++ main.py
@@ -0,0 +1,25 @@
+# fork the repl.it and create a new git hub repo entitled
+#Comparison operators
+# Logical operators
+# Decision making
+# loops(for loops, while loops, range, enumerator)
+# min/max practice
+# Random in python
+# List comprehension
+#importing modules
+#work on creating and understanding functions
+
+
+from decision_making import decisions
+from comparison_operators import comparison
+# from dictionaries_loops import dictionaries_loops
+from loops import loop
+#from list_comprehension import list_comprehension 
+#rom logical_operators import logicaloperators 
+#from min_max import min_max
+#from random import random 
+#from while_loops import while_loops
+
+decisions()
+comparison()
+loop()
